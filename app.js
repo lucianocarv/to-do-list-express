1f56a9cedf0b38004ec8de8ae7a164efeba8c5c5
Luciano Carvalho <lucianocarv13@gmail.com>
2022-10-08 19:55:47 -0300

    API com Express + MongoDB + EJS finalizada

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 const express = require("express");
 const path = require("path");
 const checklistRouter = require("./src/routes/checklist");
+const taskRouter = require("./src/routes/task");
 const rootRouter = require("./src/routes/index");
-
+const methodOverride = require("method-override");
 require("./config/database");
 
 const app = express();
 
 app.use(express.json()); // Middleware
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method", { methods: ["POST", "GET"] }));
 
 app.set("views", path.join(__dirname, "src/views"));
 app.set("view engine", "ejs");
 
 app.use("/", rootRouter); // usando um middleware personalizado
 app.use("/checklists", checklistRouter); // usando um middleware personalizado
+app.use("/checklists", taskRouter.checklistDependent); // usando um middleware personalizado
+app.use("/tasks", taskRouter.simple); // usando um middleware personalizado
 
 app.listen(3000, () => {
   console.log("Servidor Online");
--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -8,46 +8,67 @@ router.get("/", async (req, res) => {
   try {
     let checklists = await Checklist.find();
     res.status(200).render("checklists/index", { checklists: checklists });
-  } catch (err) {
+  } catch (error) {
     res.status(200).render("pages/error", { error: "Erro ao exibir as Listas" });
   }
 });
 
-router.post("/", async (req, res) => {
-  let { name } = req.body;
+router.get("/new", async (req, res) => {
   try {
-    let checklist = await Checklist.create({ name });
-    res.status(200).send(checklist);
-  } catch (err) {
-    res.status(422).json(err);
+    let checklist = new Checklist();
+    res.status(200).render("checklists/new", { checklist: checklist });
+  } catch (error) {
+    res.status(500).render("pages/error", { error: "Erro ao carregar o formulario" });
+  }
+});
+
+router.get("/:id/edit", async (req, res) => {
+  try {
+    let checklist = await Checklist.findById(req.params.id);
+    res.status(200).render("checklists/edit", { checklist: checklist });
+  } catch (error) {
+    console.log("Erro caiu no catch");
+    res.status(422).render("pages/error", { error: "Erro ao editar a lista de tarefa" });
+  }
+});
+
+router.post("/", async (req, res) => {
+  let { name } = req.body.checklist;
+  let checklist = new Checklist({ name });
+  try {
+    await checklist.save();
+    res.redirect("/checklists");
+  } catch (error) {
+    res.status(422).render("checklists/new", { checklist: { ...checklist, error } });
   }
 });
 
 router.get("/:id", async (req, res) => {
   try {
-    let checklist = await Checklist.findById(req.params.id);
+    let checklist = await Checklist.findById(req.params.id).populate("tasks");
     res.status(200).render("checklists/show", { checklist: checklist });
-  } catch (err) {
-    res.status(200).render("pages/error", { error: "Erro ao exibir a lista de tarefa" });
+  } catch (error) {
+    res.status(500).render("pages/error", { error: "Erro ao exibir a lista de tarefa" });
   }
 });
 
 router.put("/:id", async (req, res) => {
-  let { name } = req.body;
+  let { name } = req.body.checklist;
+  let checklist = await Checklist.findById(req.params.id);
   try {
-    let checklist = await Checklist.findByIdAndUpdate(req.params.id, { name }, { new: true });
-    res.status(200).json(checklist);
-  } catch (err) {
-    res.status(422).json(err);
+    await checklist.update({ name });
+    res.redirect("/checklists");
+  } catch (error) {
+    res.status(422).render("checklists/edit", { checklist: { ...checklist, error } });
   }
 });
 
 router.delete("/:id", async (req, res) => {
   try {
     let checklist = await Checklist.findByIdAndRemove(req.params.id);
-    res.status(200).json(checklist);
-  } catch (err) {
-    res.status(422).json(err);
+    res.redirect("/checklists");
+  } catch (error) {
+    res.status(500).render("pages/error", { error: "Erro ao deletar a lista de tarefa" });
   }
 });
 
