14f4d2d46853c4028c468dbdd23d04c91142514f
Luciano Carvalho <lucianocarv13@gmail.com>
2022-10-07 21:58:54 -0300

    Adicionando mais verbos http

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require("express");
 const checklistRouter = require("./src/routes/checklist");
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); // Middleware
 
-app.use("/checklists", checklistRouter);
+app.use("/checklists", checklistRouter); // usando um middleware personalizado
 
 app.listen(3000, () => {
   console.log("Servidor Online");
--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -3,12 +3,30 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send();
+  res.json({
+    route: "express",
+  });
 });
 
 router.post("/", (req, res) => {
   console.log(req.body);
   res.status(200).send(req.body);
+});
+
+router.get("/:id", (req, res) => {
+  // passando parâmetros
+  console.log(req.params.id);
+  res.send(`Id: ${req.params.id}`);
+});
+
+router.put("/:id", (req, res) => {
+  console.log(req.body);
+  res.send(`PUT Id: ${req.params.id}`);
+});
+
+router.delete("/:id", (req, res) => {
+  console.log(req.body);
+  res.send(`Delete Id: ${req.params.id}`);
 });
 
 module.exports = router;
